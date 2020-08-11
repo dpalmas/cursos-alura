008d2f75341a1827ed4e47a3d4a92455148e9a3d
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2020-08-11 17:07:59 -0300

    Add global style

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <button type="button">
+        Clica em mim
+      </button>
+    </div>
+  );
+}
+
+export default App;
